338ed539db57442ca4958cdbd8a1ca813607472a
hephhay <faradaydanfard@gmail.com>
2023-01-25 21:54:44 +0100

    fixed serilizer error

--- twitter/serializers.py
+++ twitter/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 
 class RecursiveSingleField(serializers.Serializer):
     def to_representation(self, value):
-        serializer: serializers.Serializer = self.parent.__class__(
-            value, context=self.context)  # type: ignore
+        serializer: serializers.Serializer = self.parent\
+            .__class__(value, context=self.context)  # type: ignore
         return serializer.data
